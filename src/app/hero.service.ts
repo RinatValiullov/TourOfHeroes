837e658128b62c1e70a116d590d391b9c8e88718
Rinat Valiullov <4rontender@gmail.com>
2021-10-05 17:48:07 +0500

    delete a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -78,6 +78,16 @@ export class HeroService {
     )
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(() => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('delete hero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
